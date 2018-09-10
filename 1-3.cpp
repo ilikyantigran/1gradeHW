e9ac22efa2f14db2fa75324835ca065b470fbdc8
Тигран <tigran@MacBook-Air-Tigran.local>
2018-09-10 13:18:07 +0300

    added 1-3

---
+++ 1-3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int b, c;
+	cin >> b >> c;
+	if (b != 0)
+	{
+		cout << "x = " << (-c / b) <<endl;
+	}
+	else
+	{
+
+		if (c == 0)
+		{
+			cout << "x = любому числу" << endl;
+		}
+		else
+		{
+			cout << "Нет решений" << endl;
+		}
+	}
+	return 0;
+}
