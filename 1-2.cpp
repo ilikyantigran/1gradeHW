0316ccfb16765e7572c62b95c79366389f10a356
Тигран <tigran@MacBook-Air-Tigran.local>
2018-09-10 13:15:26 +0300

    added 1-2

---
+++ 1-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << "Сумма : " <<a + b << endl;
+	cout << "Разность : " <<a - b << endl;
+	if (b!=0)
+	cout << "Деление : " <<a / b << endl;
+	else cout << "Не делите на ноль" << endl;
+	cout << "Произведение : " <<a * b << endl;
+	return 0;
+}
