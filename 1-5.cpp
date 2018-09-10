093f2e66bf3c3784c3b4649ac42d55ed8a546829
Тигран <tigran@MacBook-Air-Tigran.local>
2018-09-10 13:23:26 +0300

    added 1-5

---
+++ 1-5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+int main()
+{
+	string STOP = "";
+	int IntLight, IntDay, IntCurtain;
+	while (STOP != "Все")
+	{
+		cout << "Если лампа включена, ввести 1, нет - 2" << endl;
+		cin >> IntLight;
+		cout << "Если день, ввести 1, нет - 2" << endl;
+		cin >> IntDay;
+		cout << "Если шторы раздвинуты ввести 1, нет - 2" << endl;
+		cin >> IntCurtain;
+		if ((IntLight == 1) || (IntDay == 1 && IntCurtain == 1))
+			cout << "Светло" << endl;
+		else
+			cout << "Темно" << endl;
+		cout << "Чтобы остановиться, напишите Все" << endl;
+		cin >> STOP;
+	}
+	return 0;
+}
