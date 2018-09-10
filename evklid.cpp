4f4eb8bd8cb427502026d020ae16a114bc773ade
Тигран <tigran@MacBook-Air-Tigran.local>
2018-09-11 00:05:37 +0300

    fixed -a and -b evklid

--- evklid.cpp
+++ evklid.cpp
@@ -15,10 +15,18 @@ int a,b,del,x;
 x = 1;
 cin >> a;
 cin >> b;
+
+if (a<0 && b<0)
+  {
+    a*=-1;
+    b*=-1;
+  }
+
 if (a > b)
   del = b;
 else
   del = a;
+
 for (int i = 1; i <= del; i++)
 {
   if (a%i == 0 && b%i == 0)
