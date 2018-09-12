ef3488a0c9ac8e311d26c2d15f0131b3a8eb4905
Тигран <tigran@MacBook-Air-Tigran.local>
2018-09-12 13:35:10 +0300

    added 4-5

---
+++ 4-5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include <fstream>
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+int main()
+{
+cout << "             ...        " << endl;
+cout << "            .. ..       " << endl;
+cout << "           ..   ..      " << endl;
+cout << "          ..     ..     " << endl;
+cout << ".         ..     ..     " << endl;
+cout << " ..       ..     ..     " << endl;
+cout << "  ..     ..       ..    " << endl;
+cout << "   ..   ..         ..   " << endl;
+cout << "    .. ..           ..  " << endl;
+cout << "     ...             ..." << endl;
+  return 0;
+}
