82995016045cd4b477d46c47a2901f9d9dbcd23c
Tigran Ilikyan <tigranilikyan@gmail.com>
2018-09-17 15:30:51 +0300

    added ex52

---
+++ ex52.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <cstring>
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+int main ()
+{
+  int array[2][2];
+  cout<<"заполните первый столбец матрицы"<<endl;
+  cin >> array[0][0];
+  cin >> array [0][1];
+  cout<<"заполните второй столбец матрицы"<<endl;
+  cin >> array[1][0];
+  cin >> array [1][1];
+  cout << endl;
+  cout << array [0][0] << " " << array [0][1] << endl;
+  cout << array[1][0] << " " << array [1][1] <<endl;
+  cout<< "определитель равен: " << (array[0][0]*array[1][1])-(array[0][1]*array[1][0]) << endl;
+  return 0;
+}
